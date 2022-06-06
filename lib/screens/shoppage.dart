1a96d891ac7c7533b745982ae635ff32c5738788
Asya Brendolan <Asya@MacBook-Air-di-Asya.local>
2022-06-06 16:33:47 +0200

    aggiunta lista1

--- lib/screens/shoppage.dart
+++ lib/screens/shoppage.dart
@@ -10,12 +10,21 @@ class ShopPage extends StatelessWidget {
   Widget build(BuildContext context) {
     print('${ShopPage.routename} built');
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 20, 178, 218),
-          title: const Center(
-              child: Text(ShopPage.routename)) //ext(ShopPage.routename),
-          ),
-      backgroundColor: const Color.fromARGB(255, 179, 210, 236),
-    );
+        appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 20, 178, 218),
+            title: const Center(
+                child: Text(ShopPage.routename)) //ext(ShopPage.routename),
+            ),
+        backgroundColor: const Color.fromARGB(255, 179, 210, 236),
+        body: ListView(
+          children: [
+            ListTile(
+              //leading: Icon()
+              title: Text("Acqua"),
+              subtitle: Text("Punti: 10"),
+              trailing: Icon(Icons.arrow_right),
+            ),
+          ],
+        ));
   } //build
 } //ShopPage
